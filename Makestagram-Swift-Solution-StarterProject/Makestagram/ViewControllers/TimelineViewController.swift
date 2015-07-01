c2a5ecdbb1afaf4d144fa1b25ee6c8c1ddaaef5e
Jaimetted Olivieri <jaimettedm4@hotmail.com>
2015-07-01 14:46:58 -0700

    1-14

---
+++ Makestagram-Swift-Solution-StarterProject/Makestagram/ViewControllers/TimelineViewController.swift
@@ -0,0 +1,81 @@
+//
+//  TimelineViewController.swift
+//  Makestagram
+//
+//  Created by Jaimetted Olivieri on 6/29/15.
+//  Copyright (c) 2015 Make School. All rights reserved.
+//
+
+import UIKit
+import Parse
+class TimelineViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    var posts: [Post] = []
+    
+    var photoTakingHelper: PhotoTakingHelper?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tabBarController?.delegate = self
+    }
+    func takePhoto() {
+        // instantiate photo taking class, provide callback for when photo is selected
+        photoTakingHelper =
+            PhotoTakingHelper(viewController: self.tabBarController!) { (image: UIImage?) in
+                let post = Post()
+                post.image = image
+                post.uploadPost()
+        }
+    }
+        override func viewDidAppear(animated: Bool) {
+            super.viewDidAppear(animated)
+            
+            ParseHelper.timelineRequestforCurrentUser {
+                (result: [AnyObject]?, error: NSError?) -> Void in
+                self.posts = result as? [Post] ?? []
+                
+                for post in self.posts {
+                    let data = post.imageFile?.getData()
+                    post.image = UIImage(data: data!, scale:1.0)
+                }
+                
+                self.tableView.reloadData()
+            }
+        }
+    }
+
+// MARK: Tab Bar Delegate
+
+extension TimelineViewController: UITabBarControllerDelegate {
+    
+    func tabBarController(tabBarController: UITabBarController, shouldSelectViewController viewController: UIViewController) -> Bool {
+        
+        if (viewController is PhotoViewController) {
+            takePhoto()
+            return false
+        } else {
+            return true
+        }
+    }
+    
+}
+extension TimelineViewController: UITableViewDataSource {
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // 1
+        return posts.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // 1
+        let cell = tableView.dequeueReusableCellWithIdentifier("PostCell") as! PostTableViewCell
+        
+        // 2
+        cell.postImageView.image = posts[indexPath.row].image
+        
+        return cell
+    }
+    
+}
---
+++ Makestagram-Swift-Solution-StarterProject/Makestagram/Views/Timeline/PostTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PostTableViewCell.swift
+//  Makestagram
+//
+//  Created by Jaimetted Olivieri on 7/1/15.
+//  Copyright (c) 2015 Make School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PostTableViewCell: UITableViewCell{
+    @IBOutlet weak var postImageView: UIImageView!
+    
+    
+    
+}
